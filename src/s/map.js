00cdd149a90d687a5806750b0d50f6b9480c6eeb
Kubo2 <kelerest123@gmail.com>
2014-12-14 23:28:08 +0100

    Add transition, map-pointing and prepare labels
    @@ For now, it works.
    @@ 2014-14-12 Kubo2

--- src/s/map.js
+++ src/s/map.js
@@ -55,8 +55,72 @@
 					throw new ReferenceError();
 				}
 
+				positions = pos;
+
 				setHidden(intro);
 				setVisible(map);
+
+				var instance = this;
+				window.setTimeout(function() {
+					instance.view.call(instance, 1);
+				}, 200);
+			},
+
+			next: function() {
+				this.view(++this.label);
+			},
+
+			view: function( labelNo ) {
+				var btnNext = (function(button, ref) {
+					button.innerHTML = 'Ďalej &gt;';
+					button.onclick = function() {
+						ref.next();
+					}
+				})(window.document.createElement('button'), this);
+
+				var pos = positions[ (labelNo) - 1 ]; // 0 => x, 1 => y
+				this.moveMap.apply(this, pos);
+
+				var aLabel = document.getElementById('place-view');
+				setClass(aLabel, 'point-view'); // apply transition
+
+				aLabel.style.width = this.width;
+				aLabel.style.height = this.height;
+
+				/* show some labels about it */
+				var label;
+				label = labels.getElementsByTagName('dl')[0];
+				label = function() {
+					var h1 = label.getElementsByTagName('dt');
+
+					for(var t in h1) {
+						if(h1[t].getAttribute('data-poradie') === labelNo.toString())
+							return h1[t];
+					}
+
+					throw new Error('No enough labels');
+				}();
+
+				var theClass = label.className.split(' ')[0];
+				label = [
+					label, 															// the title label
+					label.parentNode.getElementsByClassName(theClass)[1] 	// a list
+				];
+
+				_d(label[0], label[1])
+
+				// !
+				// TODO: Insert label and the next button
+			},
+
+			moveMap: function( x, y ) {
+				x -= (this.width / 2);
+				y -= (this.height /2);
+
+				var map = document.getElementById('place-view');
+
+				map.style.backgroundPositionX = '-' + x.toString();
+				map.style.backgroundPositionY = '-' + y.toString();
 			},
 
 			// actual label
@@ -93,5 +157,13 @@
 				: cls + (' ' + className)
 			).trim();
 		;
+	}
+
+	/**
+	 * debug function
+	 */
+	function _d() {
+		for(var a in arguments)
+			console.log(arguments[a]);
 	}
 })()
